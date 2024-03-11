898814995fe783125f2687b95f0037404b2f5538
thewoodfish <jasonholt2002@gmail.com>
2024-03-11 16:24:22 +0100

    update: added more function to the php and JS files

--- script.php
+++ script.php
@@ -1,31 +1,34 @@
 <?php
 
-class Admin {
+class Admin
+{
     private $servername = "localhost";
     private $username = "buylllnf_cake143";
     private $password = 'MSmE$gg3.B!!';
     private $dbname = "buylllnf_cake143";
 
-    public function getUser($id) {
+    public function getUser($id)
+    {
         // Code to retrieve user data
     }
 
-    public function auth_admin($data) {
+    public function auth_admin($data)
+    {
         // Code to authenticate a user
         $conn = $this->connect();
-    
+
         $email = $this->sanitize($data['email']);
         $password = $data['password'];
-    
+
         // Query the database for the user's record
         $stmt = $conn->prepare("SELECT id, `name`, `type`, `password_hash` FROM users WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $stmt->bind_result($id, $name, $role, $password_hash);
         $stmt->fetch();
-    
+
         $response = [];
-    
+
         // If a record is found, compare the password hash
         if ($id !== null) {
             if ($password_hash == $this->hash_input($password)) {
@@ -33,7 +36,7 @@ class Admin {
                 // check that the user is a buylink admin
                 if ($role == "bl_admin") {
                     // Passwords match, login successful
-                    $response = (object)[
+                    $response = (object) [
                         "error" => false,
                         "data" => [
                             "id" => $id,
@@ -58,7 +61,7 @@ class Admin {
                         'samesite' => 'Lax'
                     ]);
                 } else {
-                    $response = (object)[
+                    $response = (object) [
                         "error" => true,
                         "data" => [
                             "msg" => "not an admin"
@@ -66,7 +69,7 @@ class Admin {
                     ];
                 }
             } else {
-                $response = (object)[
+                $response = (object) [
                     "error" => true,
                     "data" => [
                         "msg" => "incorrect password"
@@ -74,23 +77,23 @@ class Admin {
                 ];
             }
         } else {
-            $response = (object)[
+            $response = (object) [
                 "error" => true,
                 "data" => [
                     "msg" => "user not found"
                 ]
             ];
         }
-    
+
         // Close the statement and the connection
         $stmt->close();
         $conn->close();
-    
+
         return $response;
     }
 
     // fetch relevant data to display on dashboard
-    public function fetch_dashboard_data($data) 
+    public function fetch_dashboard_data($data)
     {
         $start_date = isset($data["start_date"]) ? $data["start_date"] : null;
         $stop_date = isset($data["stop_date"]) ? $data["stop_date"] : null;
@@ -99,10 +102,10 @@ class Admin {
 
         // Connect to your database (assuming you have a method to handle database connection)
         $conn = $this->connect();
-    
+
         // Initialize query parameters
         $params = [];
-    
+
         // Add date range conditions if start_date and stop_date are provided
         $dateRangeCondition = "";
         if ($start_date && $stop_date) {
@@ -114,7 +117,7 @@ class Admin {
         // Query to get the count of active daily users
         $activeDailyUsersQuery = "SELECT COUNT(*) AS active_daily_users FROM users WHERE modified >= CURDATE()";
         $stmt = $conn->prepare($activeDailyUsersQuery);
-        
+
         $stmt->execute();
         $stmt->bind_result($activeDailyUsers);
         $stmt->fetch();
@@ -159,7 +162,7 @@ class Admin {
         // Close the database connection
         $conn->close();
 
-        return (object)[
+        return (object) [
             "error" => false,
             "data" => [
                 'active_daily_users' => $activeDailyUsers,
@@ -171,28 +174,146 @@ class Admin {
 
     }
 
+    // fetch the paginated list of users
+    public function fetch_users($data)
+    {
+        // Connect to the database
+        $conn = $this->connect();
+
+        // Define variables
+        $page = isset($data['page']) ? intval($data['page']) : 1; // Default to page 1 if not provided
+        $limit = 50; // Number of users per page
+        $start_id = ($page - 1) * $limit; // Calculate the starting id value
+
+        // Prepare SQL query to fetch total count of users
+        $countQuery = "SELECT COUNT(*) AS total FROM users";
+        $countResult = $conn->query($countQuery);
+        if (!$countResult) {
+            return (object) ["error" => true, "message" => "Error fetching total count of users"];
+        }
+        $totalCount = $countResult->fetch_assoc()['total'];
+
+        // Prepare SQL query to fetch paginated list of users
+        $query = "SELECT `name`, email, stores_count, searches_count, modified, created FROM users ORDER BY id DESC LIMIT ?, ?";
+        $stmt = $conn->prepare($query);
+        if (!$stmt) {
+            return (object) ["error" => true, "message" => "Error preparing SQL query"];
+        }
+        $stmt->bind_param("ii", $start_id, $limit);
+        // Execute the statement
+        $stmt->execute();
+
+        // Bind result variables
+        $stmt->bind_result($name, $email, $storesCount, $searchCount, $lastSeen, $created);
+
+        // Fetch users
+        $users = [];
+        while ($stmt->fetch()) {
+            $users[] = [
+                'name' => $name,
+                'email' => $email,
+                'storesCount' => $storesCount,
+                'searchesCount' => $searchCount,
+                'lastSeen' => $lastSeen,
+                'created' => $created
+            ];
+        }
+
+        // Return paginated list of users and total count
+        return (object) [
+            "error" => false,
+            "data" => [
+                'users' => $users,
+                'totalCount' => $totalCount
+            ]
+        ];
+    }
+
+    // restrict / unrestrict an entity
+    public function restrict($data)
+    {
+        // Connect to the database
+        $conn = $this->connect();
+
+        switch ($data["type"]) {
+            case "user": {
+                // select user where email = $data["data"];
+                $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+                $stmt->bind_param("s", $data["data"]);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                $attribute = $row["_restrict"];
+                $newValue = ($attribute == "yes") ? "no" : "yes";
+                $stmt = $conn->prepare("UPDATE users SET _restrict = ? WHERE email = ?");
+                $stmt->bind_param("ss", $newValue, $data["data"]);
+                $stmt->execute();
+                break;
+            }
+            case "post": {
+                // select post where id = $data["data"];
+                $stmt = $conn->prepare("SELECT * FROM posts WHERE id = ?");
+                $stmt->bind_param("i", $data["data"]);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                $attribute = $row["_restrict"];
+                $newValue = ($attribute == "yes") ? "no" : "yes";
+                $stmt = $conn->prepare("UPDATE posts SET _restrict = ? WHERE id = ?");
+                $stmt->bind_param("si", $newValue, $data["data"]);
+                $stmt->execute();
+                break;
+            }
+            case "store": {
+                // select store where id = $data["data"];
+                $stmt = $conn->prepare("SELECT * FROM stores WHERE id = ?");
+                $stmt->bind_param("i", $data["data"]);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                $attribute = $row["_restrict"];
+                $newValue = ($attribute == "yes") ? "no" : "yes";
+                $stmt = $conn->prepare("UPDATE stores SET _restrict = ? WHERE id = ?");
+                $stmt->bind_param("si", $newValue, $data["data"]);
+                $stmt->execute();
+                break;
+            }
+        }
+
+        // Close statement and connection
+        $stmt->close();
+        $conn->close();
+
+        // Return paginated list of users and total count
+        return (object) [
+            "error" => false,
+            "data" => []
+        ];
+    }
 
     private function hash_input($password)
     {
         // hash password
         $hash1 = "CVHDGGRE%^#@%^T(HYB*WV^TBNY(WWMX*OXGXN^*TNFXVYNfcfjyj$^76TFbEIBubiKJfcskvJKJtdtrfyvu5RT^WET&Y(WI8p0WENIF2d8_QR(*W#NR)W8m)(E&)Y";
-        $hash2 = "VHdrfgvgsujuy%&^(78NHUgbyVT@Q!#3wT@ec58r&BTY(m8uy8)P()(K&Y(*YHnygihhFVJ%^&^&RB7KJFVugytfR*&%7v5Rvy679t*BN)98nun(YO|*^%NHRi6r7(";  
+        $hash2 = "VHdrfgvgsujuy%&^(78NHUgbyVT@Q!#3wT@ec58r&BTY(m8uy8)P()(K&Y(*YHnygihhFVJ%^&^&RB7KJFVugytfR*&%7v5Rvy679t*BN)98nun(YO|*^%NHRi6r7(";
 
         $hash = hash('sha256', $hash1 . sha1($password) . $hash2);
         return $hash;
     }
 
-    private function sanitize($input) {
+    private function sanitize($input)
+    {
         // Sanitize the data to prevent XSS attacks
         return htmlspecialchars($input);
-    } 
-    public function connect() {
+    }
+    public function connect()
+    {
         // Create connection
         $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-        
+
         // Check connection
         if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
+            die("Connection failed: " . $conn->connect_error);
         }
 
         return $conn;
@@ -210,23 +331,26 @@ class Admin {
 
 }
 
-class API {
+class API
+{
     private $user;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->user = new Admin();
     }
-    
-    private function check_empty_array($data) {
+
+    private function check_empty_array($data)
+    {
         // Initialize an array to store any empty field names
         $emptyFields = [];
-        
+
         foreach ($data as $fieldName => $fieldValue) {
             if (empty($fieldValue)) {
                 $emptyFields[] = $fieldName;
             }
         }
-        
+
         if (!empty($emptyFields)) {
             return (object) [
                 "error" => true,
@@ -236,22 +360,23 @@ class API {
             ];
         }
     }
-    
-    public function handleRequest() {
+
+    public function handleRequest()
+    {
         $method = $_SERVER['REQUEST_METHOD'];
         $response = "";
         $data = [];
-        
-        
+
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && stristr($_SERVER['CONTENT_TYPE'], 'multipart/form-data')) {
             $data = $_POST;
         } else {
             $jsonData = file_get_contents('php://input');
             $data = json_decode($jsonData, true);
         }
-        
+
         $endpoint = $data["endpoint"];
-        
+
         // check for empty POST data
         $this->check_empty_array($data);
 
@@ -272,16 +397,42 @@ class API {
 
                 $response = $this->user->fetch_dashboard_data($data);
                 break;
+            case 'fetch_users':
+                if (!isset($_SESSION['auth_session'])) {
+                    $response = (object) [
+                        "error" => true,
+                        "session_error" => true,
+                        "data" => [
+                            "msg" => "Invalid session"
+                        ]
+                    ];
+                }
+
+                $response = $this->user->fetch_users($data);
+                break;
+            case 'restrict':
+                if (!isset($_SESSION['auth_session'])) {
+                    $response = (object) [
+                        "error" => true,
+                        "session_error" => true,
+                        "data" => [
+                            "msg" => "Invalid session"
+                        ]
+                    ];
+                }
+
+                $response = $this->user->restrict($data);
+                break;
             default:
                 $response = (object) [
                     "error" => true,
                     "data" => [
-                        "msg" => "Invalid endpoint '". $endpoint . "'"
+                        "msg" => "Invalid endpoint '" . $endpoint . "'"
                     ]
                 ];
-                break; 
+                break;
         }
-       
+
         header('Content-Type: application/json');
         echo json_encode($response);
     }
--- src/script.js
+++ src/script.js
@@ -78,8 +78,167 @@ document.body.addEventListener("click", (e) => {
         } else {
             // toast
         }
+    } else if (e.classList.contains("pagination-btn")) {
+        // default
+        let page = localStorage["usersPageCount"];
+
+        // total user count
+        const totalCount = localStorage["usersTotalCount"];
+
+        switch (e.dataset.info) {
+            case "first": {
+                // page is always one
+                page = 1
+                localStorage["usersPageCount"] = page;
+                break;
+            }
+            case "back": {
+                // get current page
+                page = localStorage["usersPageCount"];
+                if (page > 1) {
+                    // decrease it
+                    page--;
+                }
+                break;
+            }
+            case "next": {
+                // get current index
+                let currentIndex = Math.ceil(totalCount / 50);
+
+                // prevent an overflow
+                if (page < currentIndex) {
+                    // increase it
+                    page++;
+                }
+                break;
+            }
+            case "last": {
+                // get last index
+                page = Math.ceil(totalCount / 50);
+                break;
+            }
+
+            // this is for the pagination number buttons
+            default: {
+                page = parseInt(e.dataset.info);
+            }
+        }
+
+        const url = 'https://notification.buylink.app/admin.php';
+        const postData = {
+            endpoint: "fetch_users",
+            page,
+        };
+
+        // fetch dashboard data
+        fetchData(url, postData)
+            .then(data => {
+                // Handle response data
+                if (!data.error) {
+                    // set global variable
+                    usersData = data.data.users;
+
+                    const totalCount = data.data.totalCount;
+                    const dataLen = data.data.users.length;
+
+                    let start = 50 * (page - 1);
+
+                    // entries count
+                    qs('.entries-count').innerText = `Showing ${start} - ${start + dataLen} of ${totalCount} entries`;
+
+                    // call a pure function
+                    displayUsers(usersData);
+                }
+            });
+
+    } else if (e.classList.contains("order-by")) {
+        displayUsers(orderByField(usersData, e.dataset.order));
+    } else if (e.classList.contains("restrict")) {
+        const url = 'https://notification.buylink.app/admin.php';
+        const postData = {
+            endpoint: "restrict",
+            data: e.dataset.info,
+            type: e.dataset.type
+        };
+
+        // fetch dashboard data
+        fetchData(url, postData)
+            .then(data => {
+                // Handle response data
+                if (!data.error) {
+                    // operation successful
+                    if (e.innerText == "Restrict") {
+                        e.innerText = "Unrestrict";
+                        e.className = "font-medium text-green-600 dark:text-blue-500 hover:underline restrict";
+                    } else {
+                        e.innerText = "Unrestrict";
+                        e.className = "font-medium text-red-600 dark:text-blue-500 hover:underline restrict";
+                    }
+                } else {
+                    // toast("could not complete operation")
+                }
+            });
     }
 }, false);
+
+// We'll be borrowing React style here
+let usersData = [];
+
+// a pure function
+function displayUsers(users) {
+    let index = 1;
+    qs(".table-body").innerHTML = "";
+    users.forEach(user => {
+        qs(".table-body").innerHTML +=
+            `
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="px-6 py-4">
+                    ${index}
+                </td>
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    ${user.name}
+                    </th>
+                    <td class="px-6 py-4">
+                    ${user.email}
+                    </td>
+                    <td class="px-6 py-4">
+                    ${user.searchesCount}
+                    </td>
+                    <td class="px-6 py-4">
+                    ${user.storesCount}
+                    </td>
+                    <td class="px-6 py-4">
+                    ${user.lastSeen}
+                    </td>
+                <td class="px-6 py-4">
+                    ${user.created}
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="#" class="font-medium text-red-600 dark:text-blue-500 hover:underline restrict" data-type="user" data-info=${user.email}>Restrict</a>
+                    </td>
+                </tr>
+            `;
+        index++;
+    });
+}
+
+function filterAndDisplayUsers(users, searchTerm) {
+    // Convert the search term to lowercase for case-insensitive matching
+    const search = searchTerm.toLowerCase();
+
+    // Filter the array based on the search term, storesCount, and searchesCount
+    const filteredUsers = users.filter(user => {
+        // Check if the name or email contains the search term
+        const nameMatch = user.name.toLowerCase().includes(search);
+        const emailMatch = user.email.toLowerCase().includes(search);
+
+        // Return true if any of the conditions match
+        return nameMatch || emailMatch;
+    });
+
+    // Call the display function with the filtered users
+    displayUsers(filteredUsers);
+}
 
 function loadContent(page) {
     fetch(page)
@@ -110,6 +269,65 @@ function loadContent(page) {
                             if (!data.error) {
                                 for (const key in data.data) {
                                     qs(`.${key}`).innerText = data.data[key];
+                                }
+                            } else {
+                                // if its a session error
+                                if (data.session_error) {
+                                    alert("Please login to continue");
+                                    redirectTo("login.html");
+                                }
+
+                                alert(data.data.msg);
+                            }
+                        })
+                        .catch(error => {
+                            // Handle error
+                        });
+                    break;
+                }
+                case "users.html": {
+                    // set pageCount in localStorage
+                    localStorage["usersPageCount"] = 1;
+
+                    const page = localStorage["usersPageCount"];
+                    const url = 'https://notification.buylink.app/admin.php';
+                    const postData = {
+                        endpoint: "fetch_users",
+                        page,
+                    };
+
+                    // fetch dashboard data
+                    fetchData(url, postData)
+                        .then(data => {
+                            // Handle response data
+                            if (!data.error) {
+                                // set global variable
+                                usersData = data.data.users;
+
+                                const totalCount = data.data.totalCount;
+                                const dataLen = data.data.users.length;
+
+                                // set total to localStorage
+                                localStorage["usersTotalCount"] = totalCount;
+
+                                // entries count
+                                qs('.entries-count').innerText = `Showing 1 - ${dataLen} of ${totalCount} entries`;
+
+                                // call a pure function
+                                displayUsers(usersData);
+
+                                // set up pagination buttons
+                                if (localStorage["usersPageCount"] == 1) {
+                                    let pagesCount = Math.ceil(totalCount / dataLen);
+
+                                    if (pagesCount > 1) {
+                                        const btn_index = qs(".button-pages-index");
+                                        btn_index.innerHTML = "";
+                                        for (var i = 1; i < pagesCount + 1 && i < 10; i++) {
+                                            btn_index.innerHTML +=
+                                                `<button class="pagination-btn bg-white px-4 py-2 shadow-md rounded-sm font-semibold text-gray-500" data-info="${i}">${i}</button>`;
+                                        }
+                                    }
                                 }
                             } else {
                                 // if its a session error
@@ -220,3 +438,32 @@ document.body.addEventListener("change", (e) => {
         }
     }
 }, false);
+
+
+// Register keypdown handlers
+document.body.addEventListener("keyup", (e) => {
+    e = e.target;
+
+    if (e.classList.contains("users-filter")) {
+        if (e.value) {
+            filterAndDisplayUsers(usersData, e.value);
+        } else {
+            console.log("empty");
+            // display the normal full data
+            displayUsers(usersData);
+        }
+    }
+}, false);
+
+// order array
+function orderByField(arr, field) {
+    return arr.sort((a, b) => {
+        if (a[field] < b[field]) {
+            return 1;
+        }
+        if (a[field] > b[field]) {
+            return -1;
+        }
+        return 0;
+    });
+}
